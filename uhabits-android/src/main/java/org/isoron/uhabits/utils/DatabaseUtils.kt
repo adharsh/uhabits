dcc01a582bdd821d44335bccdbd87bcd4a20a91f
Adharsh Babu <adharsh.babu@gmail.com>
2024-12-22 03:16:35 -0500

    Patch: Write to external storage

--- uhabits-android/src/main/java/org/isoron/uhabits/utils/DatabaseUtils.kt
+++ uhabits-android/src/main/java/org/isoron/uhabits/utils/DatabaseUtils.kt
@@ -18,8 +18,12 @@
  */
 package org.isoron.uhabits.utils
 
+import android.content.ContentUris
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import org.isoron.uhabits.HabitsApplication.Companion.isTestMode
 import org.isoron.uhabits.HabitsDatabaseOpener
@@ -59,6 +63,50 @@ object DatabaseUtils {
     @JvmStatic
     @Throws(IOException::class)
     fun saveDatabaseCopy(context: Context, dir: File): String {
+        val downloads_filename = "Loop_Habits_Backup.db"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Downloads.DISPLAY_NAME, downloads_filename)
+            put(MediaStore.Downloads.MIME_TYPE, "application/x-sqlite3")
+            put(MediaStore.Downloads.RELATIVE_PATH, "Download/Loop-Habits-DB")
+        }
+        // First try to find and delete any existing file
+        val selection = "${MediaStore.MediaColumns.DISPLAY_NAME} LIKE ?"
+        val selectionArgs = arrayOf("$downloads_filename%")
+        val resolver = context.contentResolver
+        Log.i("DatabaseUtils", "Checking for existing backup file...")
+        Log.i("DatabaseUtils", "Query selection: $selection")
+        Log.i("DatabaseUtils", "Query args: ${selectionArgs.joinToString()}")
+        // Query for existing file
+        resolver.query(
+            MediaStore.Downloads.EXTERNAL_CONTENT_URI,
+            arrayOf(MediaStore.Downloads._ID),
+            selection,
+            selectionArgs,
+            null
+        )?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                // File exists, delete it
+                val id = cursor.getLong(0)
+                val uri = ContentUris.withAppendedId(MediaStore.Downloads.EXTERNAL_CONTENT_URI, id)
+                val deleteCount = resolver.delete(uri, null, null)
+                Log.i("DatabaseUtils", "Found existing backup file, deleted $deleteCount files")
+            } else {
+                Log.i("DatabaseUtils", "No existing backup file found")
+            }
+        }
+        Log.i("DatabaseUtils", "Creating new backup file...")
+        // Now create the new file
+        val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+            ?: throw IOException("Failed to create new MediaStore record.")
+        Log.i("DatabaseUtils", "Writing backup data...")
+        resolver.openOutputStream(uri, "wt")?.use { outputStream ->
+            getDatabaseFile(context).inputStream().use { input ->
+                input.copyTo(outputStream)
+            }
+        } ?: throw IOException("Failed to open output stream")
+        Log.i("DatabaseUtils", "Successfully wrote backup to downloads")
+
+        // Create backup in the app's backup directory
         val dateFormat: SimpleDateFormat = getBackupDateFormat()
         val date = dateFormat.format(getLocalTime())
         val filename = "${dir.absolutePath}/Loop Habits Backup $date.db"
